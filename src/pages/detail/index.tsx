1b786071a01c12564ca4b3b90d56a1abf49b4550
1144kevin <103715483+1144kevin@users.noreply.github.com>
2024-08-14 16:32:31 +0800

    fix:detail頁的元件grid調整

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -32,19 +32,19 @@ const Detail = () => {
 
     return (
         <Row className="layout">
-            <Col xxl={12} xl={12} offset={6} className="image">
+            <Col span={12} offset={6} className="image">
                 <Image
                     width={400}
                     src="https://media.gq.com/photos/5ad64204c8be07604e8b5f2f/1:1/w_1332,h_1332,c_limit/21-books-GQ-April-2018-041718-3x2.jpg"
                 />
             </Col>
-            <Col xxl={6} xl={12} offset={9} className="title">
+            <Col span={6} offset={9} className="title">
                 <h2>{bookId?.title}</h2>
             </Col>
-            <Col xxl={10} xl={12} offset={7} className="content">
+            <Col span={14} offset={5} className="content">
                 <p>{bookId?.body}</p>
             </Col>
-            <Col xxl={10} xl={12} offset={7} className="buttonContainer">
+            <Col span={10} offset={7} className="buttonContainer">
                 <Button type="link" href="https://ant.design/index-cn">
                     更新
                 </Button>
