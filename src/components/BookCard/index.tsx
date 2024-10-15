129d954581fc81397c5102a464696cc022d352a9
kevin <s111019009@stu.ntue.edu.tw>
2024-10-15 10:25:44 +0800

    feat:change image

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -5,7 +5,7 @@ import { IoBookmarkOutline,IoBookmark } from "react-icons/io5";
 import './bookCard.scss'
 import { useDispatch } from "react-redux";
 import { addToFavorite } from "../../redux/favoriteSlice";
-import image from "https://media.gq.com/photos/5ad64204c8be07604e8b5f2f/1:1/w_1332,h_1332,c_limit/21-books-GQ-April-2018-041718-3x2.jpg";
+//import image from "https://media.gq.com/photos/5ad64204c8be07604e8b5f2f/1:1/w_1332,h_1332,c_limit/21-books-GQ-April-2018-041718-3x2.jpg";
 
 interface BookCardProps {
   book: bookDataType2;
@@ -23,12 +23,12 @@ const BookCard = ({ book }: BookCardProps) => {
           hoverable
           className="card"
           style={{ width: 240 }}
-          cover={<img alt="example" src={image} style={{ height: 240, objectFit: "cover" }} />}
+          cover={<img alt="example" src="https://media.gq.com/photos/5ad64204c8be07604e8b5f2f/1:1/w_1332,h_1332,c_limit/21-books-GQ-April-2018-041718-3x2.jpg" style={{ height: 240, objectFit: "cover" }} />}
         >
           <Meta title={book.title} description={book.body} />
         </Card>
       </Col>
-      <Col className="icon" onClick={() => dispatch(addToFavorite({book,image}))}>
+      <Col className="icon" onClick={() => dispatch(addToFavorite(book.id))}>
        <IoBookmarkOutline/>
         {/* {book.isFavorite ? (
          
